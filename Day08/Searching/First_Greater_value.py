2624ba81bc4543b77854563b74dadc5fdfbf2fde
Rosemarries <rosemarries.404notfound@gmail.com>
2022-11-07 13:27:33 +0700

    Add First Greater value

--- Day08/Searching/First_Greater_value.py
+++ Day08/Searching/First_Greater_value.py
@@ -1,14 +1,8 @@
-def find_first(l, r, listed, key, mode=False):
-    mid = (l+r)//2
-    if (mode and listed[mid] < key) or (not mode and listed[mid] > key) or (key == listed[mid] and mid+1 < len(listed)):
-        return listed[mid] if not mode and key!=listed[mid] else listed[mid+1]
-    if not mode and l==0 and listed[mid] > key:
-        mode = True
-    if l < r:
-        return find_first(l+1 if listed[mid]<key else l, r-1 if listed[mid]>key else r, listed, key, mode)
-    return "No First Greater Value"
-
 inp = input("Enter Input : ").split("/")
-list1, list2 = list(map(int, inp[0].split())), list(map(int, inp[1].split()))
+list1, list2 = sorted(list(map(int, inp[0].split()))), list(map(int, inp[1].split()))
 for e in list2:
-    print(find_first(0, len(list1)-1, sorted(list1), e))
+    try:
+        res = next(x for x, val in enumerate(list1) if val > e)
+        print(list1[res])
+    except:
+        print("No First Greater Value")
